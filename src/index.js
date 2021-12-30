6dee1c1331f6a1538365ad086d22e914fe917279
Robert Egeland <robegeland@yahoo.com>
2021-12-29 20:47:40 -0600

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    makeToDo(e.target.newTaskDescription.value)
+    form.reset()
+  })
 });
+
+function makeToDo(todo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteToDo)
+  btn.textContent = ' x '
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function deleteToDo(e) {
+  e.target.parentNode.remove()
+}
